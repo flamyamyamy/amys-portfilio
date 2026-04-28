eaed227c871a5ad718d5ba9f178db3f5fb774aee
amy <132775996+flamyamyamy@users.noreply.github.com>
2026-04-28 02:41:53 +0100

    fixigin navbar

--- amy-portfolio/src/components/Navbar.vue
+++ amy-portfolio/src/components/Navbar.vue
@@ -1,17 +1,13 @@
-<<<<<<< HEAD
 <script setup>
 import { ref } from 'vue'
 
 const menuOpen = ref(false)
 </script>
 
-=======
->>>>>>> 027cf9a (Initial local website files)
 <template>
   <nav class="navbar">
     <router-link to="/" class="logo">flamyamy</router-link>
 
-<<<<<<< HEAD
     <button
       class="menu-toggle"
       @click="menuOpen = !menuOpen"
@@ -44,30 +40,12 @@ const menuOpen = ref(false)
       <router-link to="/imprint" @click="menuOpen = false">
         Imprint
       </router-link>
-=======
-    <div class="nav-links">
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-
-      <router-link :to="{ path: '/', hash: '#skills' }">
-        Skills
-      </router-link>
-
-      <router-link :to="{ path: '/', hash: '#projects' }">
-        Projects
-      </router-link>
-
-      <router-link to="/imprint">Imprint</router-link>
->>>>>>> 027cf9a (Initial local website files)
 
       <a
         href="https://github.com/flamyamyamy"
         target="_blank"
         rel="noopener"
-<<<<<<< HEAD
         @click="menuOpen = false"
-=======
->>>>>>> 027cf9a (Initial local website files)
       >
         GitHub
       </a>
