e269f36267de51ef426678c65ea71ad29b73dd3f
amy <132775996+flamyamyamy@users.noreply.github.com>
2026-04-28 02:20:07 +0100

    Add responsive mobile hamburger navigation

--- amy-portfolio/src/components/Navbar.vue
+++ amy-portfolio/src/components/Navbar.vue
@@ -1,25 +1,51 @@
+<script setup>
+import { ref } from 'vue'
+
+const menuOpen = ref(false)
+</script>
+
 <template>
   <nav class="navbar">
     <router-link to="/" class="logo">flamyamy</router-link>
 
-    <div class="nav-links">
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
+    <button
+      class="menu-toggle"
+      @click="menuOpen = !menuOpen"
+      aria-label="Toggle navigation"
+    >
+      ☰
+    </button>
 
-      <router-link :to="{ path: '/', hash: '#skills' }">
+    <div class="nav-links" :class="{ open: menuOpen }">
+      <router-link to="/" @click="menuOpen = false">Home</router-link>
+
+      <router-link to="/about" @click="menuOpen = false">
+        About
+      </router-link>
+
+      <router-link
+        :to="{ path: '/', hash: '#skills' }"
+        @click="menuOpen = false"
+      >
         Skills
       </router-link>
 
-      <router-link :to="{ path: '/', hash: '#projects' }">
+      <router-link
+        :to="{ path: '/', hash: '#projects' }"
+        @click="menuOpen = false"
+      >
         Projects
       </router-link>
 
-      <router-link to="/imprint">Imprint</router-link>
+      <router-link to="/imprint" @click="menuOpen = false">
+        Imprint
+      </router-link>
 
       <a
         href="https://github.com/flamyamyamy"
         target="_blank"
         rel="noopener"
+        @click="menuOpen = false"
       >
         GitHub
       </a>
